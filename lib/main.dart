8150cc6d4706905fc3874fef6640181d6f841c84
Hammad Ali <hammadaali02@gmail.com>
2025-07-03 21:59:21 +0500

    Implement Kochalo login screen with Rive animations and dark theme styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rive_teddy_bear/screens/bunny_login.dart';
+import 'package:rive_teddy_bear/screens/kochalo_login.dart';
 import 'package:rive_teddy_bear/screens/man_login.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const BunnyLoginScreen(),
+      home: const KochaloLoginScreen(),
     );
   }
 }
